4432c0afd08384b7e6484ad64dd35a530705a47a
侯辛酉 <30905590@qq.com>
2013-03-01 15:04:08 +0800

    3-1

--- src/main/java/com/ht/scada/common/user/entity/User.java
+++ src/main/java/com/ht/scada/common/user/entity/User.java
@@ -8,8 +8,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-import javax.validation.constraints.Digits;
 
 import org.hibernate.annotations.Index;
 import org.springframework.data.jpa.domain.AbstractPersistable;
