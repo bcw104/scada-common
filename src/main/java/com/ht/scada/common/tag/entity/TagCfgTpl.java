bda085882394c28431da72c15690ee44033cd763
赵磊 <zhaolei-dlut@163.com>
2013-04-20 13:04:23 +0800

    1.TagCfgTpl.java添加tagName做为变量名（中文）,varName做为变量的key（英文，可重复） 2.TagCfgTpl.java枚举类型加@Enumerated(EnumType.STRING)注解 3.添加油井类型与子类型枚举 4.添加末端扩展信息的属性名字枚举 5.添加末端扩展信息属性名字——油井阶段的枚举

--- src/main/java/com/ht/scada/common/tag/entity/TagCfgTpl.java
+++ src/main/java/com/ht/scada/common/tag/entity/TagCfgTpl.java
@@ -1,8 +1,9 @@
 package com.ht.scada.common.tag.entity;
 
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Lob;
 import javax.persistence.Table;
 import javax.persistence.Transient;
@@ -14,16 +15,18 @@ import com.ht.scada.common.tag.util.DataType;
 import com.ht.scada.common.tag.util.VarGroup;
 import com.ht.scada.common.tag.util.VarType;
 
-
 /**
  * 变量词典模板<br>
- * <p>大部分的监控末端都具有相同的变量类型，所有采用通用的变量词典模板来进行定义，开端节点只需要指定模板名称即可</p>
+ * <p>
+ * 大部分的监控末端都具有相同的变量类型，所有采用通用的变量词典模板来进行定义，开端节点只需要指定模板名称即可
+ * </p>
+ * 
  * @author 薄成文
  * @author 赵磊
- *
+ * 
  */
 @Entity
-@Table(name="T_Tag_Cfg_Tpl")
+@Table(name = "T_Tag_Cfg_Tpl")
 public class TagCfgTpl extends AbstractPersistable<Integer> {
 
 	/**
@@ -31,46 +34,68 @@ public class TagCfgTpl extends AbstractPersistable<Integer> {
 	 */
 	private static final long serialVersionUID = 3948279960114335095L;
 
-	@Index(name="idx_tagcfgtpl_tplname")
-	@Column(name="tpl_name")
+	@Index(name = "idx_tagcfgtpl_tplname")
+	@Column(name = "tpl_name")
 	private String tplName;// 模板名称
-	
-	/*变量属性*/
-	@Column(name="var_name") 	private String 		varName;	// 变量名称
-	@Column(name="var_group") 		private VarGroup 	varGroup;	// 变量分组,可以为空
-	@Column(name="type") 		private VarType 	varType;	// 变量类型
-	@Column(name="sub_Type") 	private String 		subType;	// 变量子类型
-	@Column(name="data_type") 	private DataType 	dataType;	// 值类型（bool, int32, int16, bcd, mod10000, float, double）
-	
-	/*IO信息*/
-	@Column(name="fun_code") 	private int		funCode;	// 功能码
-	@Column(name="data_id") 	private int 	dataID;		// 数据ID
-	@Column(name="byte_offset") private int 	byteOffset = 0;	// 字节偏移量
-	@Column(name="bit_offset") 	private int 	bitOffset = -1;	// 位偏移量(-1表)
-	@Column(name="byte_len") 	private int 	byteLen;	// 字节长度
-	@Column(name="base_value") 	private Float 	baseValue;// 基值 
-	@Column(name="coef_value") 	private Float 	coefValue;// 系数
-	
-	/*YM信息*/
-	@Column(name="max_value") 	private Double 	max;// 最大值 
-	@Column(name="min_value") 	private Double 	min;// 最小值
-	@Column(name="unit_value") 	private Integer unit;// 遥脉单位
-	
-	/*变量扩展属性*/
-	@Column(name="comm_trigger")
-	private String trigger;		// 触发采集帧名,如"soe", 需要与采集通道中定义的帧名称对应
-	
+
+	/* 变量属性 */
+	@Column(name = "var_name")
+	private String varName; // 变量key
+	@Column(name = "tag_name")
+	private String tagName;	//变量名，中文
+	@Column(name = "var_group")
+	@Enumerated(EnumType.STRING)
+	private VarGroup varGroup; // 变量分组,可以为空
+	@Column(name = "type")
+	@Enumerated(EnumType.STRING)
+	private VarType varType; // 变量类型
+	@Column(name = "sub_Type")
+	private String subType; // 变量子类型
+	@Column(name = "data_type")
+	@Enumerated(EnumType.STRING)
+	private DataType dataType; // 值类型（bool, int32, int16, bcd, mod10000, float,
+								// double）
+
+	/* IO信息 */
+	@Column(name = "fun_code")
+	private int funCode; // 功能码
+	@Column(name = "data_id")
+	private int dataID; // 数据ID
+	@Column(name = "byte_offset")
+	private int byteOffset = 0; // 字节偏移量
+	@Column(name = "bit_offset")
+	private int bitOffset = -1; // 位偏移量(-1表)
+	@Column(name = "byte_len")
+	private int byteLen; // 字节长度
+	@Column(name = "base_value")
+	private Float baseValue;// 基值
+	@Column(name = "coef_value")
+	private Float coefValue;// 系数
+
+	/* YM信息 */
+	@Column(name = "max_value")
+	private Double max;// 最大值
+	@Column(name = "min_value")
+	private Double min;// 最小值
+	@Column(name = "unit_value")
+	private Integer unit;// 遥脉单位
+
+	/* 变量扩展属性 */
+	@Column(name = "comm_trigger")
+	private String trigger; // 触发采集帧名,如"soe", 需要与采集通道中定义的帧名称对应
+
 	/**
 	 * 存储器
+	 * 
 	 * @see com.ht.scada.common.tag.util.StorageFactory
 	 */
-	@Column(name="var_storage ")
+	@Column(name = "var_storage ")
 	@Lob
 	private String storage = "ym|0|599999999|1|0";
 
 	public TagCfgTpl() {
 	}
-	
+
 	public TagCfgTpl(Integer id) {
 		setId(id);
 	}
@@ -218,7 +243,5 @@ public class TagCfgTpl extends AbstractPersistable<Integer> {
 	public void setUnit(Integer unit) {
 		this.unit = unit;
 	}
-	
-	
-	
+
 }
