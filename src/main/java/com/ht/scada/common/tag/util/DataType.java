98531ad5f07ea15a91c2b8cf0ddc3b023642181d
薄成文 <bcw104@gmail.com>
2013-07-30 10:39:22 +0800

    增加BYTE_H,BYTE_L值类型

--- src/main/java/com/ht/scada/common/tag/util/DataType.java
+++ src/main/java/com/ht/scada/common/tag/util/DataType.java
@@ -1,7 +1,7 @@
 package com.ht.scada.common.tag.util;
 
 public enum DataType {
-	BOOL("布尔型"), INT32("整型32位"), INT16("整型16位"), BCD("BCD码"), MOD10000("MOD10000"), FLOAT("浮点型32位"), DOUBLE("双浮点型64位"), /* 遥测数组  */INT16_ARRAY("双字节数组");
+	BOOL("布尔型"), BYTE_H("高字节"), BYTE_L("低字节"), INT32("整型32位"), INT16("整型16位"), BCD("BCD码"), MOD10000("MOD10000"), FLOAT("浮点型32位"), DOUBLE("双浮点型64位"), /* 遥测数组  */INT16_ARRAY("双字节数组");
 	
 	private String value;
 
